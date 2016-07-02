27a1c68ca203a84f5a9c12cdcc727c2d5d80125b
Jeffrey King <jdking33@aol.com>
2016-07-02 04:25:30 -0700

    Installed in new enviroment; ironing out details

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  after_create :set_default_role
+ # after_create :set_default_role
 
   def standard?
     self.role == 'standard'
